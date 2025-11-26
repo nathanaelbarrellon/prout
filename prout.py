db4e6cff5e3d46b82db1f7f803e8d5694da71e38
Nathanaël Barrellon <nath@MacBook-Air-de-Nathanael.local>
2025-11-26 10:01:54 +0100

    update

--- prout.py
+++ prout.py
@@ -5,6 +5,7 @@ st.set_page_config(page_title="prout.com", page_icon="💨", layout="centered")
 # Titre
 st.title("💨 Bienvenue sur prout.com")
 
+st.write("")
 # Photo
 st.image("Screenshot 2025-11-26 at 09.33.35.png", caption="Pétronille Buthaud, CEO de prout.com")
 
@@ -14,6 +15,12 @@ st.link_button(
     "🔗 Aller donner de la force sur LinkedIn",
     "https://www.linkedin.com/in/petronille-buthaud/"
 )
+st.write("")
+st.link_button(
+    "🏃‍♀️‍➡️ Et n'hésitez pas à aller voir ma dernière perf bien pourrave :",
+    "https://www.strava.com/athletes/162133893"
+)
+
 
 st.write("---")
 st.write("Site 100% optimisé par  Nathanaël™ 💼")
